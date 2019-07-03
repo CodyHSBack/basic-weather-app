154cb9a3a42f5de7adf64455ca48062b7b0691ba
Cody H.S Back <codyback1996@gmail.com>
2019-07-03 14:59:30 -0400

    Convert between fahrenheit and celsius

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ window.addEventListener('load', ()=> {
     let temperatureDescription = document.querySelector(".temperature-description");
     let temperatureDegree = document.querySelector(".temperature-degree");
     let locationTimezone = document.querySelector(".location-timezone");
-    let temperatureSection = document.querySelector("temperature-section");
+    let temperatureSection = document.querySelector(".temperature");
+    const temperatureSpan = document.querySelector(".temperature span");
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -22,12 +23,28 @@ window.addEventListener('load', ()=> {
             .then(data =>{
                 console.log(data);
                 const {temperature, summary, icon} = data.currently;
+                
                 //Set DOM Elements from API
                 temperatureDegree.textContent = temperature;
                 temperatureDescription.textContent = summary;
                 locationTimezone.textContent = data.timezone;
+               
+                //FORUMULA FOR CELSIUS
+                let celsius = (temperature - 32) * (5 / 9);
+
                 //Set Icons
                 setIcons(icon, document.querySelector('.icon'));
+
+                //Change Temp to F/C
+                temperatureSection.addEventListener('click', () =>{
+                    if(temperatureSpan.textContent == "F"){
+                        temperatureSpan.textContent = "C";
+                        temperatureDegree.textContent = Math.floor(celsius);
+                    }else{
+                        temperatureSpan.textContent = "F";
+                        temperatureDegree.textContent = temperature;
+                    }
+                });
             });
         });
     }
